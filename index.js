16cbad4658ec997dda11789856137552b0f8c730
+ <+>
2023-10-11 21:23:52 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(event = 'roller-skate'){
+if(event==='roller-skate'){
+return 'This Saturday, I want to roller-skate!'
+}
+return `This Saturday, I want to ${event}!`
+}
+
+const mondayWork = function (work = 'go to the office'){
+    if( work ==='go to the offcie'){
+        return 'This Monday, I will go to the office.'
+    } else {
+        return `This Monday, I will ${work}.`
+    }
+}
+
+function wrapAdjective(word = "*") {
+return function innerFun() {
+    if( word === "*") {
+return `You are ${word}a hard worker${word}!`
+    } else if(word ==="||"){
+        return `You are ${word}a dedicated programmer${word}!`
+    }
+}
+}
+
